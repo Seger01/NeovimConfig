ac962fb286474109cd6098d04c7fd1ed7bbee2f7
Seger01 <sjw.sars@student.avans.nl>
2025-02-02 02:18:51 +0100

    changed latex config

--- after/plugin/latex.lua
+++ after/plugin/latex.lua
@@ -1,5 +1,22 @@
 -- Make the function global by assigning it to _G
-_G.compile_and_open_pdf = function()
+_G.compile_tex = function()
+    -- Get the full path of the current file
+    local file_path = vim.fn.expand("%:p")
+    -- Get the directory of the current file
+    local file_dir = vim.fn.expand("%:p:h")
+    -- Change the file extension to ".pdf"
+    local pdf_path = file_path:gsub("%.tex$", ".pdf")
+
+    vim.cmd(":w")
+
+    -- Compile the LaTeX file using 'latexmk'
+    vim.cmd("silent !latexmk -C")
+    vim.cmd("silent !latexmk -pdf '" .. file_path .. "'")
+    vim.cmd(":set wrap")
+end
+
+-- Make the function global by assigning it to _G
+_G.compile_tex_debug = function()
     -- Get the full path of the current file
     local file_path = vim.fn.expand("%:p")
 
@@ -10,21 +27,33 @@ _G.compile_and_open_pdf = function()
     local pdf_path = file_path:gsub("%.tex$", ".pdf")
 
     -- Compile the LaTeX file using 'latexmk'
-    vim.cmd(":setlocal spell spelllang=en_us")
-    vim.cmd(":! latexmk -pdf '" .. file_path .. "'")
-    -- vim.cmd("silent !cd " .. file_dir .. " && texliveonfly '" .. file_path .. "'")
-
-    -- Check if the PDF was successfully created
-    if vim.fn.filereadable(pdf_path) == 1 then
-        -- Open the PDF file using the preferred viewer
-        vim.cmd("silent !zathura '" .. pdf_path .. "' &")
-    else
-        print("PDF compilation failed!")
-    end
+    vim.cmd("silent !latexmk -C")
+    vim.cmd(":terminal latexmk -pdf '" .. file_path .. "'")
 end
 
+-- Make the function global by assigning it to _G
+_G.open_tex = function()
+    -- Get the full path of the current file
+    local file_path = vim.fn.expand("%:p")
+    -- Get the directory of the current file
+    local file_dir = vim.fn.expand("%:p:h")
+    -- Change the file extension to ".pdf"
+    local pdf_path = file_path:gsub("%.tex$", ".pdf")
+
+    vim.cmd("silent !evince '" .. pdf_path .. "' &")
+end
+
+-- Make the function global by assigning it to _G
+_G.spell_check = function()
+    vim.cmd(":setlocal spell spelllang=en_us")
+end
+
+
 -- Map the key (e.g., <leader>p) to compile and open the corresponding PDF
-vim.api.nvim_set_keymap("n", "<leader><leader>xx", ":lua compile_and_open_pdf()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>make", ":lua compile_tex()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ltd", ":lua compile_tex_debug()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>lto", ":lua open_tex()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader><leader>spellcheck", ":lua spell_check()<CR>", { noremap = true, silent = true })
 
 -- vim.api.nvim_create_autocmd("BufWritePost", {
 --     pattern = "*.tex",
