9c9b3f73fc63679b65a3f99ef0b71c77775cca4a
Seger01 <segersars@gmail.com>
2025-11-19 12:44:35 +0100

    made changes so treesitter parsers get preserved through sessions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,11 +20,10 @@ return {
             require('mason').setup({})
             require('mason-lspconfig').setup({
                 ensure_installed = {
-                    -- 'clangd',
-                    -- 'lua_ls',
-                    -- 'pylsp',
-                    -- 'jsonls',
-                    -- 'bashls',
+                    'clangd',
+                    'lua_ls',
+                    'jsonls',
+                    'bashls',
                 },
                 handlers = {
                     lsp.default_setup,
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,11 +7,13 @@ return {
                 ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query" },
                 sync_install = false,
                 auto_install = true,
+                parser_install_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter",
                 highlight = {
                     enable = true,
                     additional_vim_regex_highlighting = false,
                 },
             }
+            vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/nvim-treesitter")
         end,
     },
     'nvim-treesitter/playground',
