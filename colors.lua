2e71b49634e354d28f3ee27d61cc69974b8865db
Seger01 <segersars@gmail.com>
2026-02-25 15:12:33 +0100

    added lazy lock to repo

--- colors.lua
+++ colors.lua
@@ -4,27 +4,27 @@
 vim.o.background = "light"
 
 require('base16-colorscheme').setup({
-    base00 = '#16172d',
-    base01 = '#1b1c36',
-    base02 = '#30365F',
-    base03 = '#686f9a',
-    base04 = '#818596',
-    base05 = '#ecf0c1',
-    base06 = '#c1c3cc',
-    base07 = '#ffffff',
-    base08 = '#e33400',
-    base09 = '#e39400',
-    base0A = '#f2ce00',
-    base0B = '#5ccc96',
-    base0C = '#00a3cc',
-    base0D = '#7a5ccc',
-    base0E = '#b3a1e6',
-    base0F = '#ce6f8f',
+    base00 = '#f2e5bc',
+    base01 = '#ebdbb2',
+    base02 = '#d5c4a1',
+    base03 = '#bdae93',
+    base04 = '#665c54',
+    base05 = '#504945',
+    base06 = '#3c3836',
+    base07 = '#282828',
+    base08 = '#9d0006',
+    base09 = '#af3a03',
+    base0A = '#b57614',
+    base0B = '#79740e',
+    base0C = '#427b58',
+    base0D = '#076678',
+    base0E = '#8f3f71',
+    base0F = '#d65d0e',
 })
 
 -- Disable terminal background transparency for nvim
-vim.api.nvim_set_hl(0, 'Normal', { bg = '#16172d', fg = '#ecf0c1' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#16172d', fg = '#ecf0c1' })
+vim.api.nvim_set_hl(0, 'Normal', { bg = '#f2e5bc', fg = '#504945' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#f2e5bc', fg = '#504945' })
 
 local function set_hl_multiple(groups, value)
     for _, v in pairs(groups) do
@@ -33,27 +33,27 @@ local function set_hl_multiple(groups, value)
 end
 
 vim.api.nvim_set_hl(0, 'Visual', {
-    bg = '#30365F',
-    fg = '#ecf0c1',
+    bg = '#d5c4a1',
+    fg = '#504945',
 })
 
 set_hl_multiple({ 'TSComment', 'Comment' }, {
-    fg = '#686f9a',
+    fg = '#bdae93',
 })
 
 set_hl_multiple({ 'TSMethod', 'Method' }, {
-    fg = '#7a5ccc',
+    fg = '#076678',
 })
 
 set_hl_multiple({ 'TSFunction', 'Function' }, {
-    fg = '#b3a1e6',
+    fg = '#8f3f71',
 })
 
 vim.api.nvim_set_hl(0, 'Keyword', {
-    fg = '#b3a1e6',
+    fg = '#8f3f71',
 })
 
 vim.api.nvim_set_hl(0, 'MsgArea', {
-    bg = '#1b1c36',
-    fg = '#7a5ccc',
+    bg = '#ebdbb2',
+    fg = '#076678',
 })
