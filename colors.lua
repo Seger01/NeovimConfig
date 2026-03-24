19c769f29f6d7a3d319725d7426179401ffbc4d7
Seger01 <segersars@gmail.com>
2026-03-24 17:03:59 +0100

    colors

--- colors.lua
+++ colors.lua
@@ -4,27 +4,27 @@
 vim.o.background = "light"
 
 require('base16-colorscheme').setup({
-    base00 = '#1e1e2e',
-    base01 = '#313244',
-    base02 = '#45475a',
-    base03 = '#6c7086',
-    base04 = '#a6adc8',
-    base05 = '#cdd6f4',
-    base06 = '#f5e0dc',
-    base07 = '#b4befe',
-    base08 = '#f38ba8',
-    base09 = '#fab387',
-    base0A = '#f9e2af',
-    base0B = '#a6e3a1',
-    base0C = '#94e2d5',
-    base0D = '#89b4fa',
-    base0E = '#cba6f7',
-    base0F = '#f2cdcd',
+    base00 = '#f4f3ec',
+    base01 = '#e7e6df',
+    base02 = '#929181',
+    base03 = '#878573',
+    base04 = '#6c6b5a',
+    base05 = '#5f5e4e',
+    base06 = '#302f27',
+    base07 = '#22221b',
+    base08 = '#ba6236',
+    base09 = '#ae7313',
+    base0A = '#a5980d',
+    base0B = '#7d9726',
+    base0C = '#5b9d48',
+    base0D = '#36a166',
+    base0E = '#5f9182',
+    base0F = '#9d6c7c',
 })
 
 -- Disable terminal background transparency for nvim
-vim.api.nvim_set_hl(0, 'Normal', { bg = '#1e1e2e', fg = '#cdd6f4' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#1e1e2e', fg = '#cdd6f4' })
+vim.api.nvim_set_hl(0, 'Normal', { bg = '#f4f3ec', fg = '#5f5e4e' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#f4f3ec', fg = '#5f5e4e' })
 
 local function set_hl_multiple(groups, value)
     for _, v in pairs(groups) do
@@ -33,27 +33,27 @@ local function set_hl_multiple(groups, value)
 end
 
 vim.api.nvim_set_hl(0, 'Visual', {
-    bg = '#45475a',
-    fg = '#cdd6f4',
+    bg = '#929181',
+    fg = '#5f5e4e',
 })
 
 set_hl_multiple({ 'TSComment', 'Comment' }, {
-    fg = '#6c7086',
+    fg = '#6c6b5a',
 })
 
 set_hl_multiple({ 'TSMethod', 'Method' }, {
-    fg = '#89b4fa',
+    fg = '#36a166',
 })
 
 set_hl_multiple({ 'TSFunction', 'Function' }, {
-    fg = '#cba6f7',
+    fg = '#5f9182',
 })
 
 vim.api.nvim_set_hl(0, 'Keyword', {
-    fg = '#cba6f7',
+    fg = '#5f9182',
 })
 
 vim.api.nvim_set_hl(0, 'MsgArea', {
-    bg = '#313244',
-    fg = '#89b4fa',
+    bg = '#e7e6df',
+    fg = '#36a166',
 })
